59deb4ec939ba1a71c2730e00518a6ce8472ad92
Ryukkun <tkytwins2@gmail.com>
2024-11-16 16:27:20 +0900

    🎊 Initial commit

---
+++ src/main/java/fox/ryukkun_/advancementRanking/AdvancementRanking.java
@@ -0,0 +1,17 @@
+package fox.ryukkun_.advancementRanking;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancementRanking extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
