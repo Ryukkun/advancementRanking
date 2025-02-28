1990df3fe2eb0f5c620000f08ed209e6c823e9e2
Ryukkun_ <tkytwins2@gmail.com>
2025-02-28 18:30:20 +0900

    最初にロードされるクラスをMainに名前変更

--- src/main/java/fox/ryukkun_/advancementRanking/Main.java
+++ src/main/java/fox/ryukkun_/advancementRanking/Main.java
@@ -3,7 +3,7 @@ package fox.ryukkun_.advancementRanking;
 import fox.ryukkun_.advancementRanking.command.AdvancementRankingCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AdvancementRanking extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
